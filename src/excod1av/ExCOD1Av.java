b45ee11164b8a74c8c13a70d54e580fe47d50c4e
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-01-22 13:38:38 +0100

    Commit A

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package excod1av;
 
 /**
  *
- * @author damian
+ * @author 
  */
 public class ExCOD1Av {
 
