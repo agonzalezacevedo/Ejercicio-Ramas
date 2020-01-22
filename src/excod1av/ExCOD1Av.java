c3c5e9a1b81b9d738722475c035aba760982ad73
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-01-22 14:13:26 +0100

    Commit E

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -10,7 +10,7 @@ public class ExCOD1Av {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Commit B");
+        System.out.println("Commit E para hacer merge");
     }
     
 }
